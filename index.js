85cdcaa1e2d561347baec48f5fbe2ab193d62ef1
UjjawalGupta5775 <83070300+UjjawalGupta5775@users.noreply.github.com>
2021-12-02 12:24:40 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var superheroes = require("superheroes");
+var supervillains = require('supervillains');
+
+var superheroName = superheroes.random();
+var supervillains = supervillains.random();
+console.log("superhero: " + superheroName);
+console.log("supervillains: " + supervillains);
